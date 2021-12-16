d5dbe394429e0b475ad0e5742969f7ee74d0f3e8
CoKayne <80445894+CoKayne@users.noreply.github.com>
2021-12-17 00:41:48 +0800

    deleted one divider

--- cmds/music.py
+++ cmds/music.py
@@ -43,6 +43,21 @@ class Music(Cog_extension): #just testing
             vc.play(source)
 
     @commands.command()
+    async def loop(self, ctx, url):
+        if ctx.voice_client.is_playing():
+            await ctx.send("Switched songs!")
+        ctx.voice_client.stop()
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options':'-vn'}
+        YDL_OPTIONS = {'format':'bestaudio'}
+        vc = ctx.voice_client
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+            url2 = info['formats'][0]['url']
+            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+            for i in range(1,99):
+                vc.play(source)
+
+    @commands.command()
     async def pause(self, ctx):
         ctx.voice_client.pause()
         await ctx.send("Paused ⏸")
